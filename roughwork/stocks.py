b13187b8062123085d21669978c7d1383b1553f0
Marcin Kaminski <marcin442228@gmail.com>
2025-11-14 20:54:35 +0000

    stocks

--- roughwork/stocks.py
+++ roughwork/stocks.py
@@ -1,7 +1,19 @@
 
 
+# Dates and times.
+import datetime as dt
+
 # Yahoo Finance data.
 import yfinance as yf
 
-# Get historical data for multiple tickers at once.
-df = yf.download(['MSFT', 'AAPL','GOOG'], period='1mo')
+# Get data
+df = yf.download(['MSFT', 'AAPL', 'GOOG'], period='5d', interval='1h')
+
+# Current date and time.
+now = dt.datetime.now()
+
+# File name.
+filename ="../data/" + now.strftime("%Y%m%d-%H%M%S") + ".csv" 
+
+# Save data as CSV.
+df.to_csv(filename)
